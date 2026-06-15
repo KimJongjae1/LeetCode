d5a0acf52e98133349328eb7ca232fe42ce54c7a
KimJongjae1 <gghs703@naver.com>
2026-06-15 10:41:05 +0900

    Time: 2 ms (88.52%), Space: 47.7 MB (79.69%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(max<i)return false;
+            if(max>=nums.length-1)return true;
+
+            max=Math.max(max,nums[i]+i);
+        }
+        return true;
+    }
+}
