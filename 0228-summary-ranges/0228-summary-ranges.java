101571cb26d3b103981a200a9016f88e8e9053a0
KimJongjae1 <gghs703@naver.com>
2026-07-02 11:09:51 +0900

    Time: 3 ms (78.78%), Space: 46.5 MB (6.81%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -2,15 +2,10 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> list=new ArrayList<>();
         int N=nums.length;
-        if(N==0)return list;
 
         String t="";
         int bf=0;
-        if(N==1) {
-            t+=nums[0];
-            list.add(t);
-            return list;
-        }
+
         for(int i=1;i<=N;i++){
             if(i==N||nums[i-1]+1!=nums[i]){
                 t+=nums[bf];
