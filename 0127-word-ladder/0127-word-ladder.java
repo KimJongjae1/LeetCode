37038c9d8959b9de637183abf3415f066d1085e7
KimJongjae1 <gghs703@naver.com>
2026-07-06 11:44:54 +0900

    Time: 100 ms (25.03%), Space: 49.9 MB (13.09%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,54 +1,55 @@
 class Solution {
-    static List<Integer>[] list;
-    static boolean[] visit;
+    static HashSet<String> visit;
     static int end;
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        wordList.add(0,beginWord);
-        list=new ArrayList[wordList.size()];
-        end=-1;
-        for(int i=0;i<wordList.size();i++){
-            if(wordList.get(i).equals(endWord))end=i;
-            list[i]=new ArrayList<>();
+    static HashSet<String> set;
+    static class pair{
+        String str;
+        int cnt;
+
+        pair(String str,int cnt){
+            this.str=str;
+            this.cnt=cnt;
         }
-        if(end==-1)return 0;
-        visit=new boolean[wordList.size()];
-        for(int i=0;i<wordList.size();i++){
-            String str=wordList.get(i);
-           Loop: for(int k=i+1;k<wordList.size();k++){
-                String a=wordList.get(k);
-                if(str.length()!=a.length())continue;
-                int cnt=0;
-                for(int q=0;q<str.length();q++){
-                    if(str.charAt(q)!=a.charAt(q)){
-                        if(cnt==1)continue Loop;
-                        cnt++;
-                    }
-                }
-                list[i].add(k);
-                list[k].add(i);
-            }
-        }
-        
-        return BFS();
     }
-    public static int BFS(){
-        Queue<int[]> qu=new LinkedList<>();
-        visit[0]=true;
-        qu.offer(new int[]{0,1});
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        set=new HashSet<>(wordList);
+        visit=new HashSet<>();
+        
+        return BFS(beginWord,endWord);
+    }
+    public static int BFS(String beginWord,String endWord){
+        Queue<pair> qu=new LinkedList<>();
+        visit.add(beginWord);
+        qu.offer(new pair(beginWord,1));
         while(!qu.isEmpty()){
-            int[] cur=qu.poll();
+            pair cur=qu.poll();
+            String s=cur.str;
+            int c=cur.cnt;
 
-            if(cur[0]==end){
-                return cur[1];
+            if(s.equals(endWord)){
+                return c;
             }
 
-            for(int next:list[cur[0]]){
-                if(visit[next])continue;
-                visit[next]=true;
-                qu.offer(new int[]{next,cur[1]+1});
+            for(int k=0;k<26;k++){
+                char a=(char)('a'+k);
+                char[] A=s.toCharArray();
+                for(int i=0;i<s.length();i++){
+                    if(A[i]==a)continue;
+                    char temp=A[i];
+                    A[i]=a;
+                    String NEW=new String(A);
+
+                    if(set.contains(NEW)&&!visit.contains(NEW)){
+                        visit.add(NEW);
+                        qu.offer(new pair(NEW,c+1));
+                    }
+                    A[i]=temp;
+                }      
             }
+            
 
         }
         return 0;
     }
+
 }
