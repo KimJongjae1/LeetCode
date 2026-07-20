664cee77969a762c277f67d864bbbaf760f2a42f
KimJongjae1 <gghs703@naver.com>
2026-07-20 10:31:16 +0900

    Time: 1 ms (99.97%), Space: 46.6 MB (96.42%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int bit=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            bit^=nums[i];
+        }
+        return bit;
+    }
+}
