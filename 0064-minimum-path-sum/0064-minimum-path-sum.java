12885116d7d58e8ef863842cf61079a7d07e8b4f
KimJongjae1 <gghs703@naver.com>
2026-08-10 09:37:37 +0900

    Time: 5 ms (25.17%), Space: 50.2 MB (72.89%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,25 +1,20 @@
 class Solution {
-    static int N;
-    static int M;
-    static int[][] dp;
     public int minPathSum(int[][] grid) {
-        N=grid.length;
-        M=grid[0].length;
-        dp=new int[N][M];
-
-        for(int i=0;i<N;i++)
-            Arrays.fill(dp[i],-1);
-        
+        int N=grid.length;
+        int M=grid[0].length;
+        int[][] dp=new int[N][M];
         dp[0][0]=grid[0][0];
-        return DY(N-1,M-1,grid);
+        for(int i=0;i<N;i++){
+            for(int k=0;k<M;k++){
+                if(i>0&&k>0){
+                    dp[i][k]=Math.min(dp[i-1][k],dp[i][k-1])+grid[i][k];
+                }else if(i>0&&k==0){
+                    dp[i][k]=dp[i-1][k]+grid[i][k];
+                }else if(i==0&&k>0){
+                    dp[i][k]=dp[i][k-1]+grid[i][k];
+                }
+            }
+        }
+        return dp[N-1][M-1];
     }
-   public static int DY(int y,int x,int[][] grid){
-        if(x<0)return Integer.MAX_VALUE;
-        if(y<0)return Integer.MAX_VALUE;
-
-        if(dp[y][x]!=-1)return dp[y][x];
-        dp[y][x]=Math.min(DY(y-1,x,grid),DY(y,x-1,grid))+grid[y][x];
-
-        return dp[y][x];
-   } 
 }
