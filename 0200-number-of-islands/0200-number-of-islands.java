9456e4f916c367dcff5eff8a8c46a9186532c6c1
KimJongjae1 <gghs703@naver.com>
2026-09-16 11:01:33 +0900

    Time: 5 ms (29.35%), Space: 51.3 MB (88.85%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,39 +1,34 @@
 class Solution {
-    static char[][] arr;
     static int[] diy={-1,1,0,0};
     static int[] dix={0,0,-1,1};
-    static int N;
-    static int M;
     public int numIslands(char[][] grid) {
-        arr=grid;
-        N=grid.length;
-        M=grid[0].length;
-        int cnt=0;
-        for(int i=0;i<N;i++){
-            for(int k=0;k<M;k++){
-                if(arr[i][k]=='1'){
-                    BFS(i,k);
-                    cnt++;
+        int ret=0;
+        for(int i=0;i<grid.length;i++){
+            for(int k=0;k<grid[0].length;k++){
+                if(grid[i][k]=='1'){
+                    ret++;
+                    BFS(i,k,grid);
                 }
             }
         }
-        return cnt;
+        return ret;
     }
-    public static void BFS(int y,int x){
+    public static void BFS(int y,int x,char[][] grid){
         Queue<int[]> qu=new LinkedList<>();
         qu.offer(new int[]{y,x});
-        arr[y][x]='2';
+        int N=grid.length;
+        int M=grid[0].length;
         while(!qu.isEmpty()){
             int[] cur=qu.poll();
 
             for(int i=0;i<4;i++){
                 int Y=cur[0]+diy[i];
                 int X=cur[1]+dix[i];
+
                 if(Y<0||Y>=N||X<0||X>=M)continue;
-                if(arr[Y][X]=='1'){
-                    qu.offer(new int[]{Y,X});
-                    arr[Y][X]='2';
-                }
+                if(grid[Y][X]=='0')continue;
+                grid[Y][X]='0';
+                qu.offer(new int[]{Y,X});
             }
         }
     }
